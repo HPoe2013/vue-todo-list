c68357f9c1d232276435d62099797d38dee656e7
Heather Poe <poe2013@gmail.com>
2023-02-07 12:53:19 -0600

    add: poe button

---
+++ src/components/PoeButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <span @click="onClick" class="poe-button">
+        {{ text }}
+    </span>
+</template>
+
+<script>
+    export default {
+        name: 'PoeButton',
+        props: {
+            text: String,
+            color: String,
+            onClick: Function
+        },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .poe-button {
+        cursor: pointer;
+    }
+</style>
+  
