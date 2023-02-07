2208f1d31340c3bd70962ff1ed232bb121d36d26
Heather Poe <poe2013@gmail.com>
2023-02-07 13:18:08 -0600

    update: ability to edit items

--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="todo-list-item">
         <input type="checkbox" /> 
-        <span class="center">{{text}}</span>
+        <span class="center">
+            <span v-if="!isEditing">{{text}}</span>
+            <input class="edit-text" v-if="isEditing" :value="text" />
+        </span>
         <span>
-            <button class="delete-btn" @click="onDelete(this.id)">X</button>
-            <button class="edit-btn">Edit</button>
+            <button class="edit-btn" @click="editClicked($event)">{{isEditing?"Save":"Edit"}}</button>
+            <button class="delete-btn" @click="deleteClicked()">X</button>
         </span>
     </div>
 </template>
@@ -15,8 +18,24 @@
         props: {
             id: Number,
             text: String,
-            onDelete: Function
-        },
+            onDelete: Function,
+            onSave: Function
+        }, data () {
+            return {
+                isEditing: false,
+            }
+        }, methods: {
+            deleteClicked() {
+                this.isEditing 
+                    ? this.isEditing = false
+                    : this.onDelete(this.id);
+            },
+            editClicked (e) {
+                this.isEditing
+                    ? this.onSave(this.id, e.target.parentElement.parentElement.querySelector('.edit-text').value)
+                    : this.isEditing = true;
+            }
+        }
     }
 </script>
 
