dba690a5f4c15b7c1d439e394dc151b99d34af2a
Heather Poe <poe2013@gmail.com>
2023-02-07 12:53:07 -0600

    add: image rotater component

---
+++ src/components/ImageRotater.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="funny-image">
+    <img 
+      @click="rotationDeg+=90" 
+      v-bind:style="{transform: `rotate(${rotationDeg}deg)`}" 
+      alt="Fun Image" 
+      src="../assets/test-image.png"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageRotater',
+  data() {
+    return {
+        rotationDeg: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+    transition: transform 0.5s ease-in-out;
+}
+</style>
